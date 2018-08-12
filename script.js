3e46c2323bf70cfa055f275702e6ad69614eef29
Chelsea David <cryan17@cnm.edu>
2018-08-12 11:26:19 -0600

    added highlight word feature added replace word feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
 	this.classList.toggle('open')
@@ -9,5 +9,35 @@ function toggleActive(e) {
 		this.classList.toggle('open-active')
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen))
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+function changeColor(x) {
+	x.style.color = "green";
+}
+
+function changeColorBack(x) {
+	x.style.color = "purple";
+}
+
+
+function highlightOn(x) {
+	x.style.backgroundColor = "yellow";
+	}
+
+	function highlightOff(x) {
+	x.style.backgroundColor = "white";
+	}
+
+	function replaceWord(){
+	let y = document.getElementById('third').innerHTML;
+	let x = y.replace(/taco/gi, 'banana');
+	document.getElementById('third').innerHTML = x;
+}
+
+function replaceWordBack(){
+	let y = document.getElementById('third').innerHTML;
+	let x = y.replace(/banana/gi, 'taco');
+	document.getElementById('third').innerHTML = x;
+}
